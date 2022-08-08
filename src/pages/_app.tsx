96baa23c0794badc5436bb7a640bd31f7615df31
Ramish Hassan <ramishshah93@gmail.com>
2022-08-08 23:26:02 +0200

    fixed issue with app.tsx file. deleted .next folder and it started pickin up tailwind css classes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "tailwindcss/tailwind.css";
 import type { AppProps } from "next/app";
-import "../styles/globals.css";
+import "../styles/global.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
