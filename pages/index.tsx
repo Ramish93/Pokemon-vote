dcd0287fc5547e329661b17cdd6d01095731dcc8
Ramish Hassan <ramishshah93@gmail.com>
2022-08-08 21:19:56 +0200

    basic UI done

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,14 @@ import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
-    <div className="h-screen w-screen flex flex-col">
-      <div className=""></div>
-      <div className=""></div>
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="texe-2xl text-center">Which pokemon is rounder</div>
+      <div className="p-2"></div>
+      <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
+        <div className="w-16 h-16 bg-red-200" />
+        <div className="p-8">VS</div>
+        <div className="w-16 h-16 bg-red-200" />
+      </div>
     </div>
   );
 };
