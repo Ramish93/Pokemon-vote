3c683c800020b83c202cec23898f6a86aeff3b56
Ramish Hassan <ramishshah93@gmail.com>
2022-08-09 00:08:14 +0200

    error in node annot find module /node_modules/use-sync-external-store/shim/index.js is not yet solved on github

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { trpc } from "@/utils/trpc";
 
 const Home: NextPage = () => {
+  const { data } = trpc.useQuery(["hello", { text: "ramish" }]);
+  if (data) return <div>{data.greeting}</div>;
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="texe-2xl text-center">Which pokemon is rounder</div>
