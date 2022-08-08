ba3ff19821fc50b0bc0cfe76c3824ceb91d463e0
Ramish Hassan <ramishshah93@gmail.com>
2022-08-08 20:37:57 +0200

    add custom styles file

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,12 @@ import Head from "next/head";
 import Image from "next/image";
 
 const Home: NextPage = () => {
-  return <div className="first-letter:">hello world</div>;
+  return (
+    <div className="h-screen w-screen flex flex-col">
+      <div className=""></div>
+      <div className=""></div>
+    </div>
+  );
 };
 
 export default Home;
